89a933a66c71c5d0f17cd03e8486c866103ff9b5
SamBWilliams <46450744+SamBWilliams@users.noreply.github.com>
2019-07-07 19:41:20 +0100

    Added database

---
+++ StudentDB/StudentDB/Script.PostDeployment1.sql
@@ -0,0 +1,38 @@
+﻿MERGE INTO Course AS Target 
+USING (VALUES 
+        (1, 'English', 3), 
+        (2, 'Maths', 3), 
+        (3, 'Science', 4)
+) 
+AS Source (CourseID, Title, Credits) 
+ON Target.CourseID = Source.CourseID 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (Title, Credits) 
+VALUES (Title, Credits);
+
+MERGE INTO Student AS Target
+USING (VALUES 
+        (1, 'Smith', 'John', '2018-09-01'), 
+        (2, 'Jones', 'Lisa', '2018-01-13'), 
+(3, 'Carter', 'Lucy', '2017-09-03')
+)
+AS Source (StudentID, LastName, FirstName, EnrollmentDate)
+ON Target.StudentID = Source.StudentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (LastName, FirstName, EnrollmentDate)
+VALUES (LastName, FirstName, EnrollmentDate);
+
+MERGE INTO Enrollment AS Target
+USING (VALUES 
+(1, 2.00, 1, 1),
+(2, 3.50, 1, 2),
+(3, 4.00, 2, 3),
+(4, 1.80, 2, 1),
+(5, 3.20, 3, 1),
+(6, 4.00, 3, 2)
+)
+AS Source (EnrollmentID, Grade, CourseID, StudentID)
+ON Target.EnrollmentID = Source.EnrollmentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (Grade, CourseID, StudentID)
+VALUES (Grade, CourseID, StudentID);
